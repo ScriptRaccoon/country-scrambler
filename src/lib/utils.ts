f90d9b0b737d58c765462b904011563ab89df955
Script Raccoon <scriptraccoon@gmail.com>
2026-02-10 23:23:48 +0100

    scramble for real

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 export function scramble_words(input: string): string {
-	return input.split(' ').map(scramble_word).join(' ')
+	return input.split(' ').map(scramble_word_for_real).join(' ')
 }
 
 function scramble_word(input: string): string {
@@ -9,4 +9,15 @@ function scramble_word(input: string): string {
 		result = result.substring(0, i) + char + result.substring(i)
 	}
 	return result
+}
+
+function scramble_word_for_real(input: string): string {
+	if (input.length <= 1) return input
+	let attempts = 0
+	let output = input
+	while (output === input && attempts < 10) {
+		attempts++
+		output = scramble_word(input)
+	}
+	return output
 }
