e40c1a2e686a00493fe46158ff7a01b36170ae8d
Script Raccoon <scriptraccoon@gmail.com>
2026-02-14 00:27:28 +0100

    add missing translations

--- src/lib/components/ModeSelect.svelte
+++ src/lib/components/ModeSelect.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
+	import { t } from '$lib/lang.svelte'
 	import type { ModeType } from '$lib/types'
 
 	let { mode = $bindable() }: { mode: ModeType } = $props()
 </script>
 
 <div>
-	<label for="mode-select">Mode</label>
+	<label for="mode-select">
+		{t('mode')}
+	</label>
 	<select id="mode-select" bind:value={mode} class="select">
-		<option value="easy">Easy</option>
-		<option value="hard">Hard</option>
+		<option value="easy">
+			{t('easy')}
+		</option>
+		<option value="hard">
+			{t('hard')}
+		</option>
 	</select>
 </div>
 
